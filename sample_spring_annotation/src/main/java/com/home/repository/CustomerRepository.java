8a24b216431584b46c0cae204fe6041e8f811d8d
ubbn <ubbn@live.com>
2018-05-09 16:21:00 +0200

    Spring with xml & java anno

--- sample_spring_annotation/src/main/java/com/home/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.home.repository;
-
-import com.home.model.Customer;
-
-import java.util.List;
-
-public interface CustomerRepository {
-    List<Customer> findAll();
-}
--- sample_spring_annotation/src/main/java/com/home/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.home.service;
-
-import com.home.model.Customer;
-
-import java.util.List;
-
-public interface CustomerService {
-    List<Customer> findAll();
-}
